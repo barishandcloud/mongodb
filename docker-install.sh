09a5f63413d749a9cfba2d3b942aceb5d855529a
manju <cloudmeetoday@gmail.com>
2022-08-07 11:12:21 +0000

    commiting for fix later

--- docker-install.sh
+++ docker-install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 {
-#docker_installation
-apt-get remove docker docker-engine docker.io containerd runc -y
+apt-get remove docker docker-engine docker.io containerd runc ansible -y
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"	
 apt update -y
 apt-cache policy docker-ce 
 apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y 
-usermod -aG docker ${USER}       
+usermod -aG docker ${vm_user_id}
 echo "docker successfully installed"
-#docker_installation
+
+echo "Mounting file share on the vms"
+echo
+echo "Creating smbcreds dir"; mkdir -p /etc/smbcredentials
+echo
+echo "Creating smbcreds dir and populating vals"; echo "username=${sa_name}" >> /etc/smbcredentials/${sa_name}.cred
+ echo "password=${sa_key}" >> /etc/smbcredentials/${sa_name}.cred
+sudo chmod 600 /etc/smbcredentials/${sa_name}.cred
+echo
+echo "Creating file mount dir"; mkdir /mnt/fileshare
+echo "fstab entry"; echo "//${sa_name}.file.core.windows.net/fileshare /mnt/fileshare cifs nofail,credentials=/etc/smbcredentials/${sa_name}.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab
+echo
+echo "mounting the file share"; mount -t cifs //${sa_name}.file.core.windows.net/fileshare /mnt/fileshare -o credentials=/etc/smbcredentials/${sa_name}.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
+echo 
+echo "Operation complete"
 } | tee -a /tmp/logfile.txt
--- main.tf
+++ main.tf
@@ -7,6 +7,21 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
+#storage account
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "mongopocsa01062022"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "share" {
+  name                 = "fileshare"
+  storage_account_name = azurerm_storage_account.storageaccount.name
+  quota                = 50
+}
+
 # Create virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myVnet"
@@ -46,7 +61,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = "223.227.13.102"
+    source_address_prefix      = "*" #"223.186.36.170"
     destination_address_prefix = "*"
   }
   security_rule {
@@ -74,6 +89,48 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 # Create network interface
+resource "azurerm_network_interface" "b_nic" {
+  name                = "b_nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNicConfiguration"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.10"
+    public_ip_address_id          = azurerm_public_ip.vm1pip.id
+  }
+}
+
+variable "ip_addresses" {
+  default = [
+    "10.0.1.11",
+    "10.0.1.12",
+    "10.0.1.13",
+  ]
+}
+
+variable "vm_username" {
+  default = "azureuser"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name  = "nic${count.index}"
+  count = length(var.ip_addresses)
+
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "nicconfiguration${count.index}"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = element(var.ip_addresses, count.index)
+  }
+}
+
+/*
 resource "azurerm_network_interface" "nic1" {
   name                = "nic1"
   location            = azurerm_resource_group.rg.location
@@ -82,8 +139,8 @@ resource "azurerm_network_interface" "nic1" {
   ip_configuration {
     name                          = "myNicConfiguration"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm1pip.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.11"
   }
 }
 
@@ -95,7 +152,8 @@ resource "azurerm_network_interface" "nic2" {
   ip_configuration {
     name                          = "myNicConfiguration"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.12"
   }
 }
 
@@ -107,23 +165,25 @@ resource "azurerm_network_interface" "nic3" {
   ip_configuration {
     name                          = "myNicConfiguration"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.13"
   }
 }
-
-resource "azurerm_network_interface" "nic4" {
-  name                = "nic4"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "myNicConfiguration"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+*/
 
 # Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "b_nic_assoc" {
+  network_interface_id      = azurerm_network_interface.b_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_assoc" {
+  count                     = length(var.ip_addresses)
+  network_interface_id      = element(azurerm_network_interface.nic.*.id, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+/*
 resource "azurerm_network_interface_security_group_association" "nic_assoc1" {
   network_interface_id      = azurerm_network_interface.nic1.id
   network_security_group_id = azurerm_network_security_group.nsg.id
@@ -138,22 +198,19 @@ resource "azurerm_network_interface_security_group_association" "nic_assoc3" {
   network_interface_id      = azurerm_network_interface.nic3.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+*/
 
-resource "azurerm_network_interface_security_group_association" "nic_assoc4" {
-  network_interface_id      = azurerm_network_interface.nic4.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
 
 # Create virtual machine
-resource "azurerm_linux_virtual_machine" "vm1" {
-  name                  = "swarmvm1"
+resource "azurerm_linux_virtual_machine" "bastion" {
+  name                  = "bastion"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic1.id]
+  network_interface_ids = [azurerm_network_interface.b_nic.id]
   size                  = "Standard_B1s"
 
   os_disk {
-    name                 = "myOsDisk1"
+    name                 = "basOsDisk1"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -165,21 +222,81 @@ resource "azurerm_linux_virtual_machine" "vm1" {
     version   = "latest"
   }
 
-  computer_name                   = "swarmvm1"
-  admin_username                  = "azureuser"
+  computer_name                   = "mongovm"
+  admin_username                  = var.vm_username
   disable_password_authentication = true
 
   admin_ssh_key {
-    username   = "azureuser"
+    username   = var.vm_username
     public_key = file("swarmvm.pub")
   }
 }
 
+/*
 data "template_file" "script_run" {
-    template = file("docker-install.sh")
+  template = file("docker-install.sh")
+}
+*/
+
+locals {
+  vars = {
+    sa_name = azurerm_storage_account.storageaccount.name
+  }
 }
 
+
+data "template_file" "script_run" {
+  template = templatefile("docker-install.sh",
+    {
+      vm_user_id = var.vm_username,
+      sa_name    = "${azurerm_storage_account.storageaccount.name}",
+      sa_key     = "${nonsensitive(azurerm_storage_account.storageaccount.primary_access_key)}"
+    }
+  )
+}
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  depends_on = [
+    azurerm_storage_account.storageaccount
+  ]
+  count                 = length(var.ip_addresses)
+  name                  = "swarmvm${count.index}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)] #["${element(azurerm_network_interface.nic.*.id, count.index)}"] #["${azurerm_network_interface.nic[count.index].id}"] 
+  size                  = "Standard_B1s"
+
+  os_disk {
+    name                 = "OsDisk${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "swarmvm${count.index}"
+  admin_username                  = var.vm_username
+  disable_password_authentication = true
+
+  custom_data = base64encode(data.template_file.script_run.rendered)
+
+  admin_ssh_key {
+    username   = var.vm_username
+    public_key = file("swarmvm.pub")
+  }
+}
+
+/*
 resource "azurerm_linux_virtual_machine" "vm2" {
+  depends_on = [
+    azurerm_storage_account.storageaccount
+  ]
   name                  = "swarmvm2"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
@@ -187,7 +304,7 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   size                  = "Standard_B1s"
 
   os_disk {
-    name                 = "myOsDisk2"
+    name                 = "myOsDisk3"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -208,10 +325,13 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("swarmvm.pub")
-  }
+  }  
 }
 
 resource "azurerm_linux_virtual_machine" "vm3" {
+  depends_on = [
+    azurerm_storage_account.storageaccount
+  ]
   name                  = "swarmvm3"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
@@ -219,7 +339,7 @@ resource "azurerm_linux_virtual_machine" "vm3" {
   size                  = "Standard_B1s"
 
   os_disk {
-    name                 = "myOsDisk3"
+    name                 = "myOsDisk4"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -242,50 +362,4 @@ resource "azurerm_linux_virtual_machine" "vm3" {
     public_key = file("swarmvm.pub")
   }  
 }
-
-resource "azurerm_linux_virtual_machine" "vm4" {
-  name                  = "mongovm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic4.id]
-  size                  = "Standard_B1s"
-
-  os_disk {
-    name                 = "myOsDisk4"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  computer_name                   = "mongovm"
-  admin_username                  = "azureuser"
-  disable_password_authentication = true
-
-  custom_data = base64encode(data.template_file.script_run.rendered)
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("swarmvm.pub")
-  }  
-}
-
-#storage account
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = "mongopocsa01062022"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_share" "share" {
-  name                 = "fileshare"
-  storage_account_name = azurerm_storage_account.storageaccount.name
-  quota                = 50
-}
+*/
